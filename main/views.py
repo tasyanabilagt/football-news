9a12121808437d1e3aab25aa364316088c55a37f
tasyanabilagt <tasya.nabila41@ui.ac.id>
2025-09-17 16:19:20 +0700

    Complete Tutorial 3: Autentikasi, Session, Cookies, dan Selenium

--- main/views.py
+++ main/views.py
@@ -1,32 +1,50 @@
-from django.http import HttpResponse
+import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    news_list = News.objects.all()
-    
-    context = {
-        'npm' : '2406351005',
-        'name': 'Tasya Nabila Anggita Saragih',
-        'class': 'PBP F',
-        'news_list': news_list  
-    }
+    filter_type = request.GET.get("filter", "all")  # default 'all'
 
-    return render(request, "main.html", context)
+    if filter_type == "all":
+        news_list = News.objects.all()
+    else:
+        news_list = News.objects.filter(user=request.user)
+
+    context = {
+        'npm': '240123456',
+        'name': request.user.username,
+        'class': 'PBP A',
+        'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
+    }
+    return render(request, "main.html",context)
 
 def create_news(request):
     form = NewsForm(request.POST or None)
-    
-    if form.is_valid() and request.method == "POST":
-        form.save()
+
+    if form.is_valid() and request.method == 'POST':
+        news_entry = form.save(commit = False)
+        news_entry.user = request.user
+        news_entry.save()
         return redirect('main:show_main')
-        
-    context = {'form': form}
-    return render(request, 'create_news.html', context)
-    
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "create_news.html", context)
+
+@login_required(login_url='/login')
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
@@ -68,3 +86,37 @@ def show_json_by_id(request, news_id):
        return HttpResponse(json_data, content_type="application/json")
    except News.DoesNotExist:
        return HttpResponse(status=404)
+   
+def register(request):
+    form = UserCreationForm()
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    content = {'form': form}
+    return render(request, 'register.html', content)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
+    
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
